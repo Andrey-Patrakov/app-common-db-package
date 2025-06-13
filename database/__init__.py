63056f86533ecd044b502f4eca06dd60baab1187
Patrakov Andrey Vladimirovich <a.v.patrakov95@gmail.com>
2025-06-13 15:02:08 +0300

    added databse url to __init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -1,5 +1,5 @@
 from .config import db_settings
-from .utils import async_session_maker, UnitOfWork
+from .utils import async_session_maker, DATABASE_URL, UnitOfWork
 from .models import BaseDBModel
 from .repositories import AbstractRepository, SQLAlchemyRepository
 from .services import SQLAlchemyService
@@ -8,6 +8,7 @@ from .services import SQLAlchemyService
 __all__ = [
     db_settings,
     async_session_maker,
+    DATABASE_URL,
     BaseDBModel,
     AbstractRepository,
     SQLAlchemyRepository,
--- database/utils/__init__.py
+++ database/utils/__init__.py
@@ -1,8 +1,9 @@
-from .session import async_session_maker
+from .session import async_session_maker, DATABASE_URL
 from .unit_of_work import UnitOfWork
 
 
 __all__ = [
     async_session_maker,
-    UnitOfWork
+    DATABASE_URL,
+    UnitOfWork,
 ]
